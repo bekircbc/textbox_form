f1978b8f72ea9522a3d34ca799db21cbe8911213
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-10 10:08:22 +0200

    style with scss added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import "./App.scss";
 
 function App() {
   const [name, setName] = useState(``);
-  const [] = useState();
+  const [age, setAge] = useState();
   return (
     <>
       <div className="App">
         <h2>Update the page based on text typed into input fields</h2>
         <div className="formdata">
-          <div className="name">{name}</div>
+          <div className="name">Name : {name}</div>
+          <div className="age">Age : {age}</div>
         </div>
-        <form>
+        <form className="formField">
           <div className="row">
             <div className="label">Name </div>
             <div className="data">
@@ -19,6 +20,16 @@ function App() {
                 type="text"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="label">Age </div>
+            <div className="data">
+              <input
+                type="text"
+                value={age}
+                onChange={(e) => setAge(+e.target.value)}
               />
             </div>
           </div>
