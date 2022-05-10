b03df46016ed1ea2f5c7786cd54535aa642aadd6
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-10 09:51:17 +0200

    name input field created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import "./App.scss";
 
 function App() {
+  const [name, setName] = useState(``);
+  const [] = useState();
   return (
     <>
-      <div className="App">Hallo!</div>
+      <div className="App">
+        <h2>Update the page based on text typed into input fields</h2>
+        <div className="formdata">
+          <div className="name">{name}</div>
+        </div>
+        <form>
+          <div className="row">
+            <div className="label">Name </div>
+            <div className="data">
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
